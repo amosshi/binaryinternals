008452d77c289ac0676c2d01e7cd762c27772efc
amosshi <amosshi@users.noreply.github.com>
2019-06-12 14:29:19 -0700

    Change JAVA_HOME in script

--- build.sh
+++ build.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #
 
-# We will use the system default java on Ubunut Linux, could be changed if needed
+# Please change the JAVA_HOME according to the current system settings
+
+# System default java on Ubuntu Linux
 export JAVA_HOME=/usr/lib/jvm/default-java
+
+# System default java on openSUSE Linux
+#export JAVA_HOME=/usr/lib64/jvm/java
 
 mvn clean && mvn package && mvn install
 mvn javadoc:aggregate
