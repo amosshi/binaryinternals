a81e8cc74725ee65bdce9beee8a2be7827a7aaaa
amosshi <amosshi@users.noreply.github.com>
2025-07-30 00:34:31 -0700

    #4 Update libs

--- build.sh
+++ build.sh
@@ -6,8 +6,7 @@
 # Please change the JAVA_HOME according to the current system settings
 
 # Set java home on Ubuntu LTS Linux
-#export JAVA_HOME=/usr/lib/jvm/default-java
-#export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
 
 # Set java home on openSUSE Linux
 #export JAVA_HOME=/usr/lib64/jvm/java
